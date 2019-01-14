7e5ad251b2a2d80994d9688c49abc3133a4dc5a7
leejr1983 <leejr1983@yahoo.com>
2019-01-14 11:22:16 -0600

    classwork

---
+++ Problem1.js
@@ -0,0 +1,10 @@
+// Problem 1:
+// Ask the user for 5 numbers. Print the sum of the 5 numbers.
+
+number1= parseInt(prompt("Please enter number 1"))
+number2= parseInt(prompt("please enter number 2"))
+number3= parseInt(prompt("Please enter number 3"))
+number4= parseInt(prompt("Please enter number 4"))
+number5= parseInt(prompt("Please enter number 5"))
+
+console.log (number1 + number2 + number3 + number4 + number5)
